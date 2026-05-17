e3213c73c564c3804f1fd96b96b207305f123ac0
patiencenjuguna <schnyderpatience18@gmail.com>
2026-05-17 04:59:27 -0700

    Add files via upload

---
+++ UNITS1.c
@@ -0,0 +1,23 @@
+//program to display units offered this semester 
+
+/*
+Multiline comment
+Description : program to display units offered this semester
+Name:Patience Ann Wairimu 
+Reg no:BDSA-03-0017/2026
+Date:17/05/2026
+Version 1
+*/
+#include<stdio.h> //pre-processor directive ,printf() ,scanf()
+
+int main(){
+	
+	//display units offered this semester
+	printf("structured programming!!\n");
+	printf("communication skills!!\n");
+	printf("health and wellness management!!\n");
+	printf("data ethics,privacy and governance!!\n");
+	printf("basic mathematics!!\n");
+	
+	return 0;
+}	
